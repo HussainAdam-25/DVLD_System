9048c72c6790465d0b1f0674b6f5187d3ec0e8e5
EL Hussain Adam <elhussain.adam20@gmail.com>
2025-08-05 16:58:12 +0400

    Initial commit

---
+++ DVLD_Buisness/clsHashing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Buisness
+{
+    public static class clsHashing
+    {
+        public static string ComputeHash(string input)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            using (SHA256 sHA = SHA256.Create())
+            {
+                byte[] ArrayBytes = sHA.ComputeHash(Encoding.UTF8.GetBytes(input));
+                foreach (var item in ArrayBytes)
+                {
+                    stringBuilder.Append(item.ToString("x2"));
+                }
+
+                return stringBuilder.Replace("-", "").ToString();
+            }
+        }
+
+    }
+}
---
+++ DVLD_DataAccess/clsLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DVLD_DataAccess
+{
+    public static class clsLogger
+    {
+        private const string SourceApp = "DVLD";
+
+        public static void LogException(string message, EventLogEntryType entryType)
+        {
+
+            try
+            {
+                if (!EventLog.SourceExists(SourceApp))
+                    EventLog.CreateEventSource(SourceApp, "Application");
+
+                EventLog.WriteEntry(SourceApp, message, entryType);
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(SourceApp, "Exception in LogException Method: " + ex.Message, EventLogEntryType.Error);
+            }
+            
+        }
+
+        public static void LogException(Exception ex)
+        {
+            string message = $"{DateTime.Now}\n{ex.Message}\n{ex.StackTrace}";
+            LogException(message, EventLogEntryType.Error);
+        }
+
+    }
+}
